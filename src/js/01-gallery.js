13f9d53e8f9290685c292ef64f06131bee6c03b6
talask <natasha169@ukr.net>
2023-04-09 02:23:13 +0300

    add 01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
 
 console.log(galleryItems);
+
+const galleryUl = document.querySelector('.gallery');
+createGallery(galleryItems);
+function createGallery(galleryArr){
+    const newGallery = galleryArr.map(({preview, original, description}) =>{
+        return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img 
+                class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+                />
+            </a>
+        </li>
+    `;
+    }).join('');
+   
+    galleryUl.innerHTML = newGallery;
+
+}
+
+const options = {   			
+    captionsData: 'alt',
+    captionDelay: 250	
+};
+
+const lightbox = new SimpleLightbox('.gallery a', options);
