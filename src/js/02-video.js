37ff835b1ce47743154e73085e51ade04b45adfe
talask <natasha169@ukr.net>
2023-04-09 16:53:20 +0300

    add 02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,52 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    
+    player.on('timeupdate', throttle(function() {
+         
+        player.getCurrentTime().then(function(seconds) {
+
+// Варіант 1. Доробка для корректної перемотки назад           
+            // player.getSeeking().then(function(seeking) {
+            //     if(seeking){
+            //         localStorage.setItem("videoplayer-current-time", seconds);
+            //     }
+            // }).catch(function(error) {
+            //     console.log("error");
+            // });
+                
+            const savedTime = fnGetLocalStorageTime();
+
+            if(seconds > savedTime) {
+
+                localStorage.setItem("videoplayer-current-time", seconds);
+               
+            }else{
+
+                player.setCurrentTime(savedTime);
+
+            }
+
+        }).catch(function(error) {
+            console.log(error);
+        });
+           
+    }, 1000));
+
+    function fnGetLocalStorageTime(){
+        return localStorage.getItem("videoplayer-current-time") ?? 0;
+    }
+
+    // Варіант 2. Доробка для корректної перемотки назад 
+   player.on("seeking", function(){
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+       
+    }).catch(function(error) {
+        console.log(error);
+    });
+   });
